1f75ba1ac1be3a65e185262367cd9437f5a6635f
Tianshi Cheng <chengts95@163.com>
2016-04-10 21:41:26 +0800

    init

---
+++ serial_led/serial_led.ino
@@ -0,0 +1,41 @@
+/*
+  DigitalReadSerial
+ Reads a digital input on pin 2, prints the result to the serial monitor
+
+ This example code is in the public domain.
+ */
+
+// digital pin 2 has a pushbutton attached to it. Give it a name:
+int led= 12;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  // make the pushbutton's pin an input:
+  pinMode(led, OUTPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input pin:
+  int n=0;
+  while(Serial.available()){
+      if (Serial.read() == 'l'){
+             digitalWrite(led, LOW);   
+              while(Serial.read()!='n'&&n<100){
+                delay(100);
+                n++;
+                    
+                }  
+              digitalWrite(led, HIGH);    
+        
+        } 
+        delay(10);      
+  }
+  // print out the state of the button:
+    // delay in between reads for stability
+}
+
+
+
